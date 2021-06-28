8301942b88519f13f3c69b06c9561e808c8a6d31
rafikatten29 <rafikatten@hotmail.com>
2021-06-28 18:41:11 +0100

    Fix to DiscountApplier

--- src/test/java/tudelft/discount/DiscountApplierTest.java
+++ src/test/java/tudelft/discount/DiscountApplierTest.java
@@ -1,5 +1,37 @@
 package tudelft.discount;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import tudelft.invoicemocked.Invoice;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class DiscountApplierTest {
+
+    @Test
+    void checkDiscounts() {
+
+        Product oven = new Product("Electic oven", 200, "HOME");
+        Product computer = new Product("Computer", 500, "BUSINESS");
+
+        ProductDao dao = Mockito.mock(ProductDao.class);
+
+        List<Product> results = Arrays.asList(oven, computer);
+        Mockito.when(dao.all()).thenReturn(results);
+
+        DiscountApplier discountApplier = new DiscountApplier(dao);
+        discountApplier.setNewPrices();
+
+        for(Product product : dao.all()) {
+            if(product.getCategory().equals("BUSINESS")) {
+                Assertions.assertEquals(550, product.getPrice());
+            }
+            if(product.getCategory().equals("HOME")) {
+                Assertions.assertEquals(180, product.getPrice());
+            }
+        }
+    }
 
 }
